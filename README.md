8d9e77eacd7c01ac56ae1097dffcf6255c760521
root <root@mgt-development-environment-72>
2020-11-20 11:56:00 +0000

    styling

---
+++ README.md
@@ -0,0 +1,5 @@
+# Portfolio website
+
+by  <a href="https://github.com/shroomgaze" rel="shroomgaze">Yohan</a>
+
+ <a href="https://shroomgaze.github.io/" rel="shroomgaze">![](img/banner.jpg)</a>
---
+++ js/load.js
@@ -0,0 +1,56 @@
+/*
+ *
+ * akshay sharma 
+ * fxn769@gmail.com
+ * 5/7/2020
+ * 
+ */
+
+var socialtags = [{
+    img: "img/github.svg",
+    link: "https://github.com/shroomgaze"
+}, {
+    img: "img/linkedin.svg",
+    link: "https://www.linkedin.com/in/yohansyahck/"
+}, {
+    img: "img/gitlab.svg",
+    link: "https://gitlab.com/shroomgaze"
+}, {
+    img: "img/twitter.svg",
+    link: "https://twitter.com/shroomgaze"
+}]
+
+function loadSocialIcons() {
+    var socialtext = ""
+    for (x in socialtags) {
+        var data = `<a class="center" href="` + socialtags[x].link + `"> <img class="center" style="padding: 5px!important;width: 40px;" src="` + socialtags[x].img + `"></a>`
+        socialtext += data
+    }
+    document.getElementById("social-list").innerHTML = socialtext;
+}
+
+loadSocialIcons()
+
+function dayNightToggle() {
+    var element = document.body;
+    element.classList.toggle("light-mode");
+    var check = window.localStorage.getItem('light-mode');
+    if (check == "light") {
+        window.localStorage.clear();
+    } else {
+        window.localStorage.setItem('light-mode', 'light');
+    }
+
+}
+
+function checkDayNight() {
+    var check = window.localStorage.getItem('light-mode');
+    var element = document.body;
+    if (check == "light") {
+        element.classList.add("light-mode");
+    } else {
+        element.classList.remove("light-mode");
+    }
+}
+
+checkDayNight()
